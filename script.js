e23721cbb38cf074295fe7d691c06e990c936fc9
Darek <48442595+DariuszSikorra@users.noreply.github.com>
2019-04-08 13:16:30 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector(".location-timezone");
+  let temperatureSection = document.querySelector(".degree-section");
+  const temperatureSpan = document.querySelector(".degree-section span");
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}https://api.darksky.net/forecast/fa253c1ede25b79cdc93e652089f8042/${lat},${long}`;
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+          const { temperature, summary, icon } = data.currently;
+          //Set DOM elements from API
+          temperatureDegree.textContent = temperature;
+          temperatureDescription.textContent = summary;
+          locationTimezone.textContent = data.timezone;
+          //Change value F to C
+          let celcius = (temperature - 32) * (5 / 9);
+          //Set icon
+          setIcons(icon, document.querySelector(".icon"));
+          //change F to C
+          temperatureSection.addEventListener("click", () => {
+            if (temperatureSpan.textContent === "F") {
+              temperatureSpan.textContent = "C";
+              temperatureDegree.textContent = Math.floor(celcius);
+            } else {
+              temperatureSpan.textContent = "F";
+              temperatureDegree.textContent = temperature;
+            }
+          });
+        });
+    });
+  } else {
+    h1.textContent =
+      "Hey, its no working, becouse I cannt  find your geolocation";
+  }
+
+  function setIcons(icon, iconId) {
+    const skycons = new Skycons({ color: "white" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconId, Skycons[currentIcon]);
+  }
+});
